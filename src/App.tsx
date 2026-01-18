7bdcd6f5c2bf65f29d61fc309fe91507b911c48c
gyanas <gyanasl@venteur.com>
2026-01-18 21:54:03 +0545

    Initial course content setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GitWorkflow from './pages/GitWorkflow'
+
+function App() {
+  return (
+    <>
+      <h1>This repository is under construction</h1>
+      <GitWorkflow />  
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/GitWorkflow.tsx
@@ -0,0 +1,16 @@
+const GitWorkflow = () => {
+    return (
+        <>
+            <h1>Git Workflow Page - Under Construction</h1>
+            <p>Things you need to do to create repo for your project</p>
+            <ol>
+                <li>Create a new repository on GitHub</li>
+                <ol>
+                    <li>Install github cli and login</li>
+                </ol>
+            </ol>
+        </>
+    )
+}
+
+export default GitWorkflow;
