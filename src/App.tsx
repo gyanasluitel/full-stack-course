5d7eddc978072f923e94eb8b3346fcef0586aa64
gyanas <gyanasl@venteur.com>
2026-01-26 16:58:04 +0545

    Update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import './App.css'
-import { Route, Routes } from 'react-router'
-import Home from './pages/Home'
-import GitWorkflow from './pages/git/GitWorkflow'
-import GitReviseAndBranching from './pages/git/GitReviseAndBranching'
-import TypeScriptTutorial from './pages/typeScript/TypeScriptTutorial'
+import { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router';
+import Home from './pages/Home';
+import GitWorkflow from './pages/git/GitWorkflow';
+import GitReviseAndBranching from './pages/git/GitReviseAndBranching';
+import TypeScriptTutorial from './pages/typeScript/TypeScriptTutorial';
 
 function App() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+    if (redirect) {
+      let path = redirect.replace(/^\/full-stack-course/, '');
+      if (path.startsWith('/')) path = path.slice(1);
+      navigate('/' + path, { replace: true });
+    }
+  }, [navigate]);
+
   return (
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="git-github" element={<GitWorkflow />}>Git & GitHub</Route>
-        <Route path="git-revise-branching" element={<GitReviseAndBranching />}>Git Revise & Branching</Route>
-        <Route path="type-script-tutorial" element={<TypeScriptTutorial />}>TypeScript Tutorial</Route>
-      </Routes>
-  )
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="git-github" element={<GitWorkflow />}>Git & GitHub</Route>
+      <Route path="git-revise-branching" element={<GitReviseAndBranching />}>Git Revise & Branching</Route>
+      <Route path="type-script-tutorial" element={<TypeScriptTutorial />}>TypeScript Tutorial</Route>
+    </Routes>
+  );
 }
 
 export default App
